589c06f8efe040989a408f88ec9f573cb7891c37
Christine <christinesunnjm@gmail.com>
2021-07-14 10:10:28 -0400

    display platform icon based on user selection

--- contented/Views/TaskCell.m
+++ contented/Views/TaskCell.m
@@ -22,16 +22,48 @@
 
 - (void)setTask:(Task *)task {
     _task = task;
-    
     self.titleLabel.text = self.task.title;
     
-    // configure image views
-//    self.imageView1.image = nil;
-//    self.imageView2.image = nil;
-//    self.imageView3.image = nil;
-//    self.imageView4.image = nil;
+    NSArray *imageViews = [NSArray array];
+    imageViews = [imageViews arrayByAddingObject:self.imageView1];
+    imageViews = [imageViews arrayByAddingObject:self.imageView2];
+    imageViews = [imageViews arrayByAddingObject:self.imageView3];
+    imageViews = [imageViews arrayByAddingObject:self.imageView4];
+    __block int index = 0;
     
-    // TODO: traverse the dictionary and set the image based on 0 or 1 
+    // Display platform icons depending on user selection for this task
+    NSDictionary *dict = self.task.platforms;
+    [dict enumerateKeysAndObjectsUsingBlock:^(id _Nonnull key, id _Nonnull obj, BOOL * _Nonnull stop) {
+        // key is social media platform, obj is TRUE or FALSE
+        NSString *thisValue = obj;
+        NSInteger valueInt = [thisValue integerValue];
+        if (valueInt == 1) {
+            UIImage *image = [self findImage:key];
+            UIImageView *currentView = [imageViews objectAtIndex:index];
+            [currentView setImage:image];
+            index++;
+        }
+    }];
+    
+    // Set remaining image view slots to nil
+    for (int i = index; i < 4; i++) {
+        UIImageView *currentView = imageViews[i];
+        currentView.image = nil;
+    }
+}
+
+- (UIImage*)findImage:(NSString*)platform  {
+    if ([platform isEqualToString:@"YouTube"])
+        return [UIImage imageNamed:@"youtube_icon"];
+    else if ([platform isEqualToString:@"Instagram"])
+        return [UIImage imageNamed:@"instagram_icon"];
+    else if ([platform isEqualToString:@"Snapchat"])
+        return [UIImage imageNamed:@"snapchat_icon"];
+    else if ([platform isEqualToString:@"TikTok"])
+        return [UIImage imageNamed:@"tiktok_icon"];
+    else if ([platform isEqualToString:@"Twitter"])
+        return [UIImage imageNamed:@"twitter_icon"];
+    else return nil;
 }
 
 @end
