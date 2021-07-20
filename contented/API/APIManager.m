dd795fd7d0f419b3668c1777cafab9135ee9fb1a
Christine <christinesunnjm@gmail.com>
2021-07-20 12:16:17 -0400

    made fetching views more efficient by doing network call within loop

--- contented/API/APIManager.m
+++ contented/API/APIManager.m
@@ -16,6 +16,7 @@ NSString* API_KEY = @"AIzaSyDqMCcWcGl3kQdFPI-CskwwFcm0N4CsU-8"; // should hide
    
     // Get the last 20 videos from this user
     NSString *baseString = [NSString stringWithFormat:@"https://www.googleapis.com/youtube/v3/search?key=%@&channelId=%@&part=snippet,id&order=date&maxResults=20", API_KEY, userID];
+    
     __block NSDictionary *initialDictionary = [[NSDictionary alloc] init];
     NSURL *url = [NSURL URLWithString:baseString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
@@ -40,36 +41,57 @@ NSString* API_KEY = @"AIzaSyDqMCcWcGl3kQdFPI-CskwwFcm0N4CsU-8"; // should hide
                     video.title = title;
                     video.vidID = videoId;
                     video.publishedAt = published;
+                    //
+                    NSString *baseString = [NSString stringWithFormat:@"https://www.googleapis.com/youtube/v3/videos?part=statistics&id=%@&key=%@", videoId, API_KEY];
+                    NSURL *url = [NSURL URLWithString:baseString];
+                    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+                    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+                    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                            if (error != nil) {
+                                NSLog(@"%@", [error localizedDescription]);
+                            }
+                            else {
+                                NSDictionary *videoDict = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                                NSArray *items = videoDict[@"items"];
+                                NSDictionary *middle = items[0];
+                                NSDictionary *stats = middle[@"statistics"];
+                                NSString *viewCount = stats[@"viewCount"];
+                                video.views = [viewCount integerValue];
+                                NSLog(@"%@ %@ %d", video.title, video.publishedAt, video.views);
+                            }
+                    }];
+                    [task resume];
+                    //
                     [vids addObject:video];
                 }
             }
-            
-            for (int i = 0; i < [vids count]; i++) {
-                // call the network request to get the number of views for this
-                Video *video = vids[i];
-                NSString *videoID = video.vidID;
-                
-                NSString *baseString = [NSString stringWithFormat:@"https://www.googleapis.com/youtube/v3/videos?part=statistics&id=%@&key=%@", videoID, API_KEY];
-                NSURL *url = [NSURL URLWithString:baseString];
-                NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
-                NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
-                NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                        if (error != nil) {
-                            NSLog(@"%@", [error localizedDescription]);
-                        }
-                        else {
-                            NSDictionary *videoDict = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-                            NSArray *items = videoDict[@"items"];
-                            NSDictionary *middle = items[0];
-                            NSDictionary *stats = middle[@"statistics"];
-                            NSString *viewCount = stats[@"viewCount"];
-                            video.views = [viewCount integerValue];
-                            NSLog(@"%@ %@ %d", video.title, video.publishedAt, video.views);
-                        }
-                }];
-                [task resume];
-            }
         }
+            
+//            for (int i = 0; i < [vids count]; i++) {
+//                // call the network request to get the number of views for this
+//                Video *video = vids[i];
+//                NSString *videoID = video.vidID;
+//
+//                NSString *baseString = [NSString stringWithFormat:@"https://www.googleapis.com/youtube/v3/videos?part=statistics&id=%@&key=%@", videoID, API_KEY];
+//                NSURL *url = [NSURL URLWithString:baseString];
+//                NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+//                NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+//                NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//                        if (error != nil) {
+//                            NSLog(@"%@", [error localizedDescription]);
+//                        }
+//                        else {
+//                            NSDictionary *videoDict = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+//                            NSArray *items = videoDict[@"items"];
+//                            NSDictionary *middle = items[0];
+//                            NSDictionary *stats = middle[@"statistics"];
+//                            NSString *viewCount = stats[@"viewCount"];
+//                            video.views = [viewCount integerValue];
+//                            NSLog(@"%@ %@ %d", video.title, video.publishedAt, video.views);
+//                        }
+//                }];
+//                [task resume];
+        
     }];
     [task resume];
     return 0;
