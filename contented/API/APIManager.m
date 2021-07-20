9295ceab687fc72b3e1b8e4c9bd308babebbe112
Christine <christinesunnjm@gmail.com>
2021-07-20 12:25:05 -0400

    setVideoProperties in helper method

--- contented/API/APIManager.m
+++ contented/API/APIManager.m
@@ -10,6 +10,7 @@
 
 @implementation APIManager
 
+NSMutableArray *vids;
 NSString* API_KEY = @"AIzaSyDqMCcWcGl3kQdFPI-CskwwFcm0N4CsU-8"; // should hide
 
 + (NSDictionary*) fetchLast20Views: (NSString*) userID {
@@ -27,49 +28,47 @@ NSString* API_KEY = @"AIzaSyDqMCcWcGl3kQdFPI-CskwwFcm0N4CsU-8"; // should hide
         }
         else {
             initialDictionary = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-            NSMutableArray *vids = [[NSMutableArray alloc] init];
+            vids = [[NSMutableArray alloc] init];
             NSArray *videos = initialDictionary[@"items"];
             for (int i = 0; i < videos.count; i++) {
-                NSDictionary *thisVideo = videos[i];
-                NSDictionary *ids = thisVideo[@"id"];
-                NSString *videoId = ids[@"videoId"];
-                if (videoId != nil) {
-                    Video *video = [[Video alloc] init];
-                    NSDictionary *snippet = thisVideo[@"snippet"];
-                    NSString *title = snippet[@"title"];
-                    NSString *published = snippet[@"publishedAt"];
-                    video.title = title;
-                    video.vidID = videoId;
-                    video.publishedAt = published;
-                    //
-                    NSString *baseString = [NSString stringWithFormat:@"https://www.googleapis.com/youtube/v3/videos?part=statistics&id=%@&key=%@", videoId, API_KEY];
-                    NSURL *url = [NSURL URLWithString:baseString];
-                    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
-                    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
-                    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                            if (error != nil) {
-                                NSLog(@"%@", [error localizedDescription]);
-                            }
-                            else {
-                                NSDictionary *videoDict = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-                                [self setVideoViews:video:videoDict];
-//                                NSArray *items = videoDict[@"items"];
-//                                NSDictionary *middle = items[0];
-//                                NSDictionary *stats = middle[@"statistics"];
-//                                NSString *viewCount = stats[@"viewCount"];
-//                                video.views = [viewCount integerValue];
-                                NSLog(@"%@ %@ %d", video.title, video.publishedAt, video.views);
-                            }
-                    }];
-                    [task resume];
-                    //
-                    [vids addObject:video];
-                }
+                [self setVideoProperties:videos[i]];
+                
             }
         }
     }];
     [task resume];
     return 0;
+}
+
++ (void)setVideoProperties: (NSDictionary*)thisVideo {
+    NSDictionary *ids = thisVideo[@"id"];
+    NSString *videoId = ids[@"videoId"];
+    if (videoId != nil) {
+        Video *video = [[Video alloc] init];
+        NSDictionary *snippet = thisVideo[@"snippet"];
+        NSString *title = snippet[@"title"];
+        NSString *published = snippet[@"publishedAt"];
+        video.title = title;
+        video.vidID = videoId;
+        video.publishedAt = published;
+        
+        NSString *baseString = [NSString stringWithFormat:@"https://www.googleapis.com/youtube/v3/videos?part=statistics&id=%@&key=%@", videoId, API_KEY];
+        NSURL *url = [NSURL URLWithString:baseString];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+        NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                if (error != nil) {
+                    NSLog(@"%@", [error localizedDescription]);
+                }
+                else {
+                    NSDictionary *videoDict = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                    [self setVideoViews:video:videoDict];
+                    NSLog(@"%@ %@ %d", video.title, video.publishedAt, video.views);
+                }
+        }];
+        [task resume];
+        [vids addObject:video];
+    }
 }
 
 + (void)setVideoViews: (Video*)video: (NSDictionary*)videoDict {
